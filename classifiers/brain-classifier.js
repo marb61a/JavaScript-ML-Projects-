a74239400c345baf64ae691b89d6e66869462d17
Martin <marb61a@gmail.com>
2021-01-01 12:20:22 +0000

    Added tokenise.js and added basic brain-classifier

---
+++ classifiers/brain-classifier.js
@@ -0,0 +1,11 @@
+class BrainClassifier {
+    train(corpus){
+
+    }
+
+    process(utterance){
+        
+    }
+}
+
+module.exports = BrainClassifier();
--- classifiers/tokenise.js
+++ classifiers/tokenise.js
@@ -1,4 +1,5 @@
 const natural = require('natural');
+const { TokenizerEn } = require('@nlpjs/lang-en');
 
 const input = "I didn't finish the task. I cannot end today. I'll end tomorrow";
 
@@ -10,15 +11,29 @@ function tokenize(text){
 }
 
 function tokenizeAggresive(text){
-
+    const tokenizer = new natural.AggressiveTokenizer();
+    return tokenizer.tokenize(text);
 }
 
 function tokenizeTreebank(text){
-    
+    const tokenizer = new natural.TreebankWordTokenizer();
+    return tokenizer.tokenize(text);
+}
+
+// Will normalize the string, differently to above
+// i'll ->  will become -> I will, the true value will change
+// certain values to uppercase eg i to I
+function tokenizeNlpjs(text) {
+    const tokenizer = new TokenizerEn();
+    return tokenizer.tokenize(text, true);
 }
 
 console.log(tokenize(input));
 
 // Special characters will be considered as whitespaces using the word split approach so Quién
 // will be returned as 2 words Qui and n, with normalization this will not happen
-console.log(tokenize("Quién"));
+// console.log(tokenize("Quién"));
+
+console.log(tokenizeAggresive(input));
+console.log(tokenizeTreebank(input));
+console.log(tokenizeNlpjs(input));
