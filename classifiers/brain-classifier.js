86d3dde294f8370ff9cfc8c49ab9c25ea2db5d0c
Martin <marb61a@gmail.com>
2021-01-03 11:46:44 +0000

    Updated classifiers brain classifier and renamed

--- classifiers/brain-classifier.js
+++ classifiers/brain-classifier.js
@@ -1,2 +1,5 @@
 const BrainClassifier = require('./classifiersexamples/brain-classifier');
 const corpus = require('./data/corpus-en.json');
+
+const classifier = new BrainClassifier();
+classifier.train(corpus);
--- classifiers/classifiersexamples/01-brain-classifier.js
+++ classifiers/classifiersexamples/01-brain-classifier.js
@@ -1,17 +1,32 @@
 const { CorpusLookup } = require("@nlpjs/utils");
 const { TokenizerEn } = require('@nlpjs/lang-en');
+const { NeuralNetwork } = require("brain.js");
+
+const defaultNetOptions = {
+    log: (str) => console.log(str),
+    logPeriod: 10,
+    hiddenLayers: [],
+    activation: "leaky-relu",
+    learningRate: 0.7,
+    momentum: 0.5,
+    alpha: 0.08,
+    errorThresh: 0.00005
+};
 
 class BrainClassifier {
-    constructor(){
+    constructor(settings){
+        this.settings = settings || defaultNetOptions;
         this.tokenizer = new TokenizerEn();
-        this.stemmer = {
+        this.stemmer = settings.stemmer || {
             tokenizeAndStem: (str) => this.tokenizer.tokenize(str, true)
         };
     }
 
     train(corpus){
         this.lookups = new CorpusLookup(corpus, this.stemmer);
+        // console.log(this.lookups);
         
+        this.net = new NeuralNetwork();
     }
 
     process(utterance){
@@ -19,7 +34,7 @@ class BrainClassifier {
     }
 }
 
-module.exports = BrainClassifier();
+module.exports = BrainClassifier;
 
 // Greeting intents
 // good morning                 -> [1, 1, 0, 0, 0, 0, 0]
