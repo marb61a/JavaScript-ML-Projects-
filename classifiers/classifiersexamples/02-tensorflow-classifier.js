50d61ebdaefb333b2bb90c49245d47662892ea8c
Martin <marb61a@gmail.com>
2021-01-04 11:54:15 +0000

    Updated classifiersexamples tensorflow classifier file

--- classifiers/classifiersexamples/02-tensorflow-classifier.js
+++ classifiers/classifiersexamples/02-tensorflow-classifier.js
@@ -1,6 +1,7 @@
 const tf = require('@tensorflow/tfjs-node');
 const { CorpusLookup } = require('@nlpjs/utils');
 const { TokenizerEn } = require('@nlpjs/lang-en');
+const { mod } = require('@tensorflow/tfjs-node');
 
 class TensorflowClassifier{
     constructor(settings, stemmer) {
@@ -9,7 +10,29 @@ class TensorflowClassifier{
         this.stemmer = stemmer || { tokenizeAndStem: (str) => this.tokenizer.tokenize(str, true) };
     }
 
-    createModel(){
+    createModel(numFeatures, numClasses){
+        const model = tf.sequential();
+        model.add({
+            inputShape: [numFeatures],
+            activation: "linear",
+            units: numClasses
+        });
+        model.add(tf.layers.softmax);
+
+        model.compile({
+            optimizer: tf.train.adagrad(0.5),
+            loss: "categoricalCrossentropy"
+        });
+
+        return model;
+    }
+
+    getTrainingData(){
+        const result = {
+            xs: [],
+            ys: []
+        };
+
         
     }
 
