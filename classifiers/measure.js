83752db9d57c791a13b6e5602d3662169b78c374
Martin <marb61a@gmail.com>
2021-01-07 20:07:26 +0000

    Updated measure.js file for measuring classifiers

--- classifiers/measure.js
+++ classifiers/measure.js
@@ -6,7 +6,24 @@ const NlpjsClassifier = require('./classifiersexamples/03-nlpjs-classifier');
 const OwnClassifier = require('./classifiersexamples/06-own-classifier');
 
 async function measure(classifier){
+    let total = 0;
+    let good = 0;
 
+    for(let i = 0; i < corpus.data.length; i += 1){
+        const item = corpus.data[i];
+        for(let j = 0; j < item.tests.length; i += 1){
+            const test = item.tests[j];
+            const output = await classifier.process(test);
+            total += 1;
+            const intent = Array.isArray(output) ? output[0].intent : output.intent;
+
+            if(intent === item.intent){
+                good += 1;
+            }
+        }
+    }
+
+    return { total, good, name: classifier.constructor.name };
 }
 
 async function main(){
@@ -23,7 +40,18 @@ async function main(){
     classifiers.push(new OwnClassifier());
     classifiers.push(nlp);
     const outputs = [];
-    
+
+    for(let i = 0; i < classifiers.length; i += 1){
+        const classifier = classifiers[i];
+        await classifier.train(corpus);
+        const output = await measure(classifier);
+        outputs.push(output);
+    }
+
+    for(let i = 0; i < outputs.length; i += 1){
+        const { name, good, total } = outputs[i];
+        console.log(`${good} good from a total of ${total} which is ${good * 100 / total}%`);
+    }
 }
 
 main();
