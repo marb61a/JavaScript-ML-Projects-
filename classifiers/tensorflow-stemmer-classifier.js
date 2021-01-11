7dbc9054ad162e770b9ffb264b6517df8c1c085d
Martin <marb61a@gmail.com>
2021-01-11 19:27:39 +0000

    Added tensorflow stemmer classifier file

---
+++ classifiers/tensorflow-stemmer-classifier.js
@@ -0,0 +1,26 @@
+const { StemmerEn } = require('@nlpjs/lang-en');
+const BrainClassifier = require('./classifiersexamples/02-tensorflow-classifier');
+const corpus = require('./data/corpus-en.json');
+
+async function main(){
+    const stemmer = new StemmerEn();
+    const classifier = new BrainClassifier(undefined, stemmer);
+    await classifier.train(corpus);
+
+    let total = 0;
+    let good = 0;
+
+    corpus.data.forEach(item => {
+        item.tests.forEach(test => {
+            const classifications = classifier.process(test);
+            total += 1;
+            if (classifications[0].intent === item.intent) {
+                good += 1;
+            }
+        });
+    });
+
+    console.log(`${good}  good from a total of ${total} which is ${good * 100 / total}%`);
+}
+
+main();
