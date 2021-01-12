6b20a2f4b98fdb2317db8b395e571b754a6f5aaa
Martin <marb61a@gmail.com>
2021-01-12 18:37:01 +0000

    Updated measure stemmer classifier file

--- classifiers/stemmingexamples/measure-stemmer-classifier.js
+++ classifiers/stemmingexamples/measure-stemmer-classifier.js
@@ -1,11 +1,32 @@
 const { dockStart } = require('@nlpjs/basic');
 const { StemmerEn } = require('@nlpjs/lang-en');
-const corpus = require('./data/corpus-en.json');
+const corpus = require('../data/corpus-en.json');
 
 const BrainClassifier = require('../classifiersexamples/01-brain-classifier');
 const TensorflowClassifier = require('../classifiersexamples/02-tensorflow-classifier');
 const NlpjsClassifier = require('../classifiersexamples/03-nlpjs-classifier');
 const OwnClassifier = require('../classifiersexamples/06-own-classifier');
+
+async function measure(classifier){
+    let total = 0;
+    let good = 0;
+
+    for(let i = 0; i < corpus.data.length; i += 1){
+        const item = corpus.data[i];
+        for(let j = 0; j < item.tests.length; i += 1){
+            const test = item.tests[j];
+            const output = await classifier.process(test);
+            total += 1;
+            const intent = Array.isArray(output) ? output[0].intent : output.intent;
+
+            if(intent === item.intent){
+                good += 1;
+            }
+        }
+    }
+
+    return { total, good, name: classifier.constructor.name };
+}
 
 async function main(){
     const dock = await dockStart({
@@ -22,7 +43,18 @@ async function main(){
     classifiers.push(new OwnClassifier(undefined, stemmer));
     classifiers.push(nlp);
     const outputs = [];
-    
+
+    for(let i = 0; i < classifiers.length; i += 1){
+        const classifier = classifiers[i];
+        await classifier.train(corpus);
+        const output = await measure(classifier);
+        outputs.push(output);
+    }
+
+    for(let i = 0; i < outputs.length; i += 1){
+        const { name, good, total } = outputs[i];
+        console.log(`${name} - ${good}  good from a total of ${total} which is ${good * 100 / total}%`);
+    }
 }
 
 main();
